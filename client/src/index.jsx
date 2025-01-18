56789e0d5fc8b8c2506ae8fb4747c29baea6fec4
Christopher Chan <christopherchan@dhcp-10-29-146-11.dyn.MIT.EDU>
2025-01-17 23:05:15 -0500

    changed client ids

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,21 +8,21 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from 'react-router-dom'
+  RouterProvider,
+} from "react-router-dom";
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-//TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "FILL ME IN";
+//TODO: DONE - REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "99548937462-ju96p61ngch1n4qt79iq076ltvjnfcv6.apps.googleusercontent.com";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route errorElement={<NotFound />} element={<App />}>
-      <Route path="/" element={<Skeleton />}/>
+      <Route path="/" element={<Skeleton />} />
     </Route>
   )
-)
+);
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.createRoot(document.getElementById("root")).render(
--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL ME IN";
+const CLIENT_ID = "99548937462-ju96p61ngch1n4qt79iq076ltvjnfcv6.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
