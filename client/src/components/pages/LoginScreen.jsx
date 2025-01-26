d31e67104c9a48dbb0d77a24520a35ace4e89e35
Christopher Chan <christopherchan@MacBook-Pro-16.local>
2025-01-26 16:13:54 -0500

    login pngs

--- client/src/components/pages/LoginScreen.jsx
+++ client/src/components/pages/LoginScreen.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import "./LoginScreen.css";
+import loginBg from "../../assets/login.png";
+import login1 from "../../assets/login1.png";
+import login2 from "../../assets/login2.png";
 
 const LoginScreen = ({ handleLogin }) => {
   return (
-    <div className="LoginScreen-container">
+    <div className="LoginScreen-container" style={{ backgroundImage: `url(${loginBg})` }}>
       <div className="login-screen">
         <h1>Welcome to Dreamscape</h1>
         <p>Log in to start generating and logging your dreams.</p>
@@ -13,8 +16,8 @@ const LoginScreen = ({ handleLogin }) => {
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       </div>
       <div className="cloud-background">
-        <div className="cloud-layer cloud-layer-2"></div>
-        <div className="cloud-layer cloud-layer-1"></div>
+        <div className="cloud-layer cloud-layer-2" style={{ backgroundImage: `url(${login2})` }}></div>
+        <div className="cloud-layer cloud-layer-1" style={{ backgroundImage: `url(${login1})` }}></div>
       </div>
     </div>
   );
